511a30f6012129fee7d7e5336a0303d6ee0fb6fb
Akshay Mathur <akshaymathur@statusneo.com>
2025-01-21 18:34:48 +0530

    Refactored backend code to be in line with web UI workflow.

---
+++ VMS/src/test/java/com/statusneo/vms/TestVisitorApplication.java
@@ -0,0 +1,10 @@
+package com.statusneo.vms;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestVisitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(VmsApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+}
