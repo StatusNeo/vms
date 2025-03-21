a0145327e297fb7bfc26cbd12ba50d0bc9f0be01
Anas Khan <rishu.singh@mahindra.com>
2025-03-21 11:54:16 +0530

    New changes made

---
+++ src/main/java/com/statusneo/vms/repository/VisitorRepository.java
@@ -0,0 +1,13 @@
+package com.statusneo.vms.repository;
+
+import com.statusneo.vms.model.Visitor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface VisitorRepository extends JpaRepository<Visitor, Long> {
+//	List<Visitor> findAllByVisitDateBetween(LocalDateTime startDate, LocalDateTime endDate);
+	Optional<Visitor> findByPhoneNumber(String phoneNumber);
+}
---
+++ src/main/java/com/statusneo/vms/service/VisitService.java
@@ -0,0 +1,73 @@
+package com.statusneo.vms.service;
+
+import com.statusneo.vms.model.Visit;
+import com.statusneo.vms.model.Visitor;
+import com.statusneo.vms.repository.VisitRepository;
+import com.statusneo.vms.repository.VisitorRepository;
+import jakarta.mail.MessagingException;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class VisitService {
+
+    @Autowired
+    private OtpService otpService;
+
+    @Autowired
+    private NotificationService notificationService;
+
+    @Autowired
+    private VisitorRepository visitorRepository;
+
+    @Autowired
+    private EmailService emailService;
+
+    @Autowired
+    private VisitRepository visitRepository;
+
+    // Register visit with OTP
+    public Visit registerVisit(Visit visit) {
+        visit.setOtp(otpService.generatedOtp());
+        visit.setVisitDate(LocalDateTime.now());
+
+        // Save visitor details
+        Visit id = visitRepository.save(visit);
+        return id;
+
+        // Send OTP
+//        notificationService.sendOtp(visitor.getEmail(), visitor.getOtp());
+    }
+
+    @Transactional
+    public Visitor saveVisitor(Visitor visitor) {
+        Visitor savedVisitor = visitorRepository.save(visitor);
+        emailService.sendVisitorEmail(savedVisitor);
+        otpService.sendOtp(savedVisitor.getEmail());
+        return savedVisitor;
+    }
+
+
+    public Visitor registerVisitor(Visitor visitor) {
+        // Save visitor
+        Visitor savedVisitor = visitorRepository.save(visitor);
+
+        try {
+            // Send Excel report to admin automatically
+            emailService.sendVisitorData("arshu.rashid.khan@gmail.com");  // Change to the recipient email
+        } catch (MessagingException | IOException e) {
+            e.printStackTrace(); // Log the error
+        }
+
+        return savedVisitor;
+    }
+
+    public List<Visitor> getAllVisitors() {
+        return visitorRepository.findAll();
+    }
+}
