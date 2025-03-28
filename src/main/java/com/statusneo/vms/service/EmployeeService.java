0433abc4ce961948f4d86ef72acdcf93c8ae91cc
Anas Khan <rishu.singh@mahindra.com>
2025-03-28 17:21:45 +0530

    New changes made as PR comments

---
+++ src/main/java/com/statusneo/vms/service/EmployeeService.java
@@ -0,0 +1,26 @@
+package com.statusneo.vms.service;
+
+import com.statusneo.vms.model.Employee;
+import com.statusneo.vms.repository.EmployeeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+    private static final Logger logger = LoggerFactory.getLogger(EmployeeService.class);
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public List<Employee> searchEmployeesByName(String prefix) {
+        logger.info("Searching employees with name starting with: {}", prefix);
+        List<Employee> employees = employeeRepository.findByNameStartingWith(prefix);
+        logger.info("Found {} employees matching prefix '{}'", employees.size(), prefix);
+        return employees;
+    }
+}
