822513fc1de936a4c964a8160bc3e43323c9187f
anurag <anurag.s@statusneo.com>
2025-07-21 12:13:58 +0530

    added comments for author and privacy policy

--- web-backend/src/main/java/com/statusneo/vms/service/GraphEmailService.java
+++ web-backend/src/main/java/com/statusneo/vms/service/GraphEmailService.java
@@ -1,3 +1,16 @@
+/* ********************************************************************
+ *
+ *  Copyright (c) 2025 Statusneo.
+ *
+ * All rights Reserved.
+ *
+ * Redistribution and use of any source or binary or in any form, without
+ * written approval and permission is prohibited.
+ *
+ * Please read the Terms of use, Disclaimer and Privacy Policy on https://www.statusneo.com/
+ *
+ * **********************************************************************
+ */
 package com.statusneo.vms.service;
 
 import com.statusneo.vms.model.Email;
