7e773273b89563b4b4fd68312e7968468b6c8edf
anuragsharma <anurag.s@gmail.com>
2025-11-13 14:21:20 +0530

    DirectorySync Result change

--- web-backend/src/test/java/com/statusneo/vms/service/ScheduledTasksSchedulingITest.java
+++ web-backend/src/test/java/com/statusneo/vms/service/ScheduledTasksSchedulingITest.java
@@ -2,6 +2,7 @@ package com.statusneo.vms.service;
 
 import com.statusneo.vms.VmsApplication;
 import com.statusneo.vms.config.TestRestTemplateConfig;
+import com.statusneo.vms.dto.DirectorySyncResult;
 import org.awaitility.Awaitility;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,8 +10,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static org.mockito.Mockito.*;
@@ -32,9 +35,18 @@ class ScheduledTasksSchedulingITest
     @MockitoSpyBean
     private ScheduledTasks scheduledTasks;
 
+
+    @MockitoBean
+    private GraphDirectoryService graphDirectoryService;
+
+
     @BeforeEach
     void setUp() {
         clearInvocations(scheduledTasks);
+
+        DirectorySyncResult mockResult = new DirectorySyncResult(0, "dummy-delta-link");
+
+        when(graphDirectoryService.syncAllUsersToEmployees()).thenReturn(mockResult);
     }
 
     @Test
